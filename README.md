ac374b6deda7740fd3644b8fa1a2122183ba6f9a
Thomas Burman <thomasburman@Thomass-MacBook-Pro.local>
2016-01-10 21:17:42 +0000

    added config values and readme

---
+++ README.md
@@ -0,0 +1,1 @@
+###U Under Construction - 10/01/2016
--- app/code/community/JustTom/BitbucketConnect/controllers/AccessController.php
+++ app/code/community/JustTom/BitbucketConnect/controllers/AccessController.php
@@ -5,20 +5,17 @@ class JustTom_BitbucketConnect_AccessController
 {
     public function clientAction()
     {
-        $test = new League\OAuth2\Client\Provider\GenericProvider();
         $bitbucketProvider = new Stevenmaguire\OAuth2\Client\Provider\Bitbucket(
             array(
                 'clientId' => Mage::getStoreConfig('justtom_bitbucketconnect/global/client_id'),
                 'clientSecret' => Mage::getStoreConfig('justtom_bitbucketconnect/global/client_password'),
-                'redirectUri' => 'http://aff75f77.ngrok.io/index.php/bitbucket/access/callback'
+                'redirectUri' => Mage::getStoreConfig('justtom_bitbucketconnect/global/redirect_uri')
             )
         );
-
-        $dave = '';
     }
 
     public function callbackAction()
     {
-        $dave = '';
+
     }
 }
