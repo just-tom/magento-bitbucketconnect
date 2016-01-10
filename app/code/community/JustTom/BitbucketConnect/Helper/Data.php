b8b53a11ef9ead17842f5c29b6e4936887feb172
Thomas Burman <thomasburman@Thomass-MacBook-Pro.local>
2016-01-10 17:37:30 +0000

    initial commit

---
+++ app/code/community/JustTom/BitbucketConnect/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasburman
+ * Date: 08/01/2016
+ * Time: 17:02
+ */ 
+class JustTom_BitbucketConnect_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/community/JustTom/BitbucketConnect/sql/justtom_bitbucketconnect_setup/install-1.0.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasburman
+ * Date: 08/01/2016
+ * Time: 17:02
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
